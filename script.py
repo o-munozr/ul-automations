5ea8cbb91d3fe5e4a8a622a234ebc9a3a2c84677
o-munozr <osvaldo.munoz@ul.com>
2026-02-19 00:49:06 -0600

    Update script.py

--- script.py
+++ script.py
@@ -38,15 +38,16 @@ cookies = json.loads(decoded)
 # =========================
 
 options = Options()
+options.binary_location = "/usr/bin/google-chrome"
+
 options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-gpu")
 options.add_argument("--window-size=1920,1080")
+options.add_argument("--remote-debugging-port=9222")
 
 driver = webdriver.Chrome(options=options)
-wait = WebDriverWait(driver, 60)
-
-driver.get(BASE_URL)
 
 wait.until(
     lambda d: d.execute_script("return document.readyState") == "complete"
