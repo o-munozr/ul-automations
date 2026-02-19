459b4f4467fb6da42860f653198f8daafeb3f237
o-munozr <osvaldo.munoz@ul.com>
2026-02-19 00:18:29 -0600

    Update script.py

--- script.py
+++ script.py
@@ -38,8 +38,8 @@ cookies = json.loads(decoded)
 # =========================
 
 options = Options()
+options.add_argument("--headless=new")
 options.add_argument("--disable-blink-features=AutomationControlled")
-options.add_argument("--start-maximized")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--window-size=1920,1080")
